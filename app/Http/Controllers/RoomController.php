e14649b90b7d67539fe6c550adcfab5496a8d0bb
Alex Livingston <aslivingston@hotmail.co.uk>
2024-03-25 14:23:48 +0000

    created get all action and updated API routing

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
+use App\Services\JsonResponseService;
 use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
-    //
+    private JsonResponseService $responseService;
+
+    public function __construct(JsonResponseService $responseService)
+    {
+        $this->responseService = $responseService;
+    }
+    public function all()
+    {
+        $hidden = ['description', 'rate'];
+
+        return response()->json($this->responseService->getFormat(
+            'Rooms successfully retrieved',
+            Room::with('type: id, name')->get()->makeHidden($hidden)
+        ));
+    }
 }
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    public $hidden = ['created_at', 'updated_at'];
+
 }
