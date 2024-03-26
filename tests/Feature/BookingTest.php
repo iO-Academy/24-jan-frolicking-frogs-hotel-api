ecc3f3aa7153915487555bfc4ebd669e0e24c355
Victoria McCusker <vrmm@hotmail.co.uk>
2024-03-26 11:24:07 +0000

    creating testing

--- tests/Feature/BookingTest.php
+++ tests/Feature/BookingTest.php
@@ -16,23 +16,33 @@ class BookingTest extends TestCase
 
     public function test_getAllBookingsSuccess(): void
     {
-        Booking::factory()->create();
+        Booking::factory()->has(Room::factory())->create();
+
 
         $response = $this->getJson('/api/bookings');
+
         $response->assertStatus(200)
             ->assertJson(function (AssertableJson $json) {
                 $json->hasAll(['message', 'data'])
+                    ->whereType('message', 'string')
                     ->has('data', 1, function (AssertableJson $json){
-                        $json->hasAll(['id', 'customer', 'start', 'end', 'rooms'])
+                        $json->hasAll(['id', 'customer', 'guests', 'start', 'end', 'rooms'])
                             ->whereAllType([
                                 'id' => 'integer',
                                 'customer' => 'string',
+                                'guests' => 'integer',
                                 'start' => 'string',
-                                'end' => 'string',
+                                'end' => 'string'
+                            ])
+                            ->has('room', 1, function (AssertableJson $json){
+                                $json->hasAll(['id', 'name'])
+                                    ->whereAllType([
+                                        'id' => 'integer',
+                                        'name' => 'string'
+                                    ]);
 
-                            ]);
+                            });
                     });
             });
     }
-
 }
